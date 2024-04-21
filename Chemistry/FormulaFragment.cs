a2de24a972ce692e7e4e730b3627aeb742ad39c5
destroY17 <eykheandrey@mail.ru>
2024-04-21 19:40:39 +0300

    refactor

--- Chemistry/FormulaFragment.cs
+++ Chemistry/FormulaFragment.cs
@@ -2,9 +2,9 @@
 
 public class FormulaFragment(string chemicalElement, int count)
 {
-    public string ChemicalElement { get; } = chemicalElement;
+    public string ChemicalElement => chemicalElement;
 
-    public int Count { get; } = count;
+    public int Count => count;
 
     public override string ToString() => $"{ChemicalElement}:{Count}";
 }
